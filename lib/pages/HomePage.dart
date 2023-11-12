c9c88a7d8a1b665c98635209684cf34a781e7409
Chamath <chamathharshana989@gmail.com>
2023-11-12 19:44:21 +0530

    Update HomePage.dart

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -25,6 +26,66 @@ class _HomePageState extends State<HomePage> {
             colorFilter: ColorFilter.mode(
                 Colors.black.withOpacity(0.33), BlendMode.darken),
           ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: AppBar().preferredSize.height,
+            ),
+
+            //Location and Menu Row
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Row(
+                children: [
+                  const Icon(
+                    Icons.location_on,
+                    color: Colors.white,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'Paris',
+                    style: GoogleFonts.roboto(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Spacer(),
+                  const Icon(
+                    Icons.menu,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(
+              height: 50,
+            ),
+
+            //Date
+            Text(
+              'June 07',
+              style: GoogleFonts.roboto(
+                fontSize: 40,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
+
+            //Updated Date and Time
+            Text(
+              'Updated 6/7/2023 4:55 PM',
+              style: GoogleFonts.roboto(
+                fontSize: 16,
+                fontWeight: FontWeight.w300,
+                color: Colors.white,
+              ),
+            ),
+          ],
         ),
       ),
     );
