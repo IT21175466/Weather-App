c2f402d9d83257f2a07e178c027ffd8b9eb9c424
Chamath <chamathharshana989@gmail.com>
2023-11-23 12:39:55 +0530

    Update weather_provider.dart

--- lib/provider/weather_provider.dart
+++ lib/provider/weather_provider.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/repositories/weather_repository.dart';
 
 class WeatherProvider extends ChangeNotifier {
+  bool isLoaded = false;
   var _repo = WeatherRepo();
   var data;
 
   info(String city) async {
     try {
       data = await _repo.getData(city);
+      isLoaded = true;
 
       notifyListeners();
       return data;
