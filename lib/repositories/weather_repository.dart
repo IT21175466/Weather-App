61b9e753181148fa6281cd7afabe0043aa0423a8
Chamath <chamath@flexiata.lk>
2024-02-16 10:29:42 +0530

    Done

--- lib/repositories/weather_repository.dart
+++ lib/repositories/weather_repository.dart
@@ -6,7 +6,7 @@ import 'package:weather_app/model/weather_model.dart';
 class WeatherRepo {
   Future<Weather> getData(String city) async {
     var uriCall = Uri.parse(
-        'http://api.weatherapi.com/v1/forecast.json?key=0298c5385ad74cc4bc4134353230212&q=$city&days=5&aqi=no&alerts=no');
+        'http://api.weatherapi.com/v1/forecast.json?key=e7be673a307a430195243109241602&q=$city&days=5&aqi=no&alerts=no');
     var response = await http.get(uriCall);
     var body = jsonDecode(response.body);
 
